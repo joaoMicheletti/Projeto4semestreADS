02e02f964b0a940524d4d6eeb29ee442f31df9d5
Micheletti <jv604014@gmail.com>
2023-10-12 23:03:25 -0300

    finalizada a etapa de login

--- projetoADS/pages/login/login.php
+++ projetoADS/pages/login/login.php
@@ -1,5 +1,46 @@
 <?php
 $email = $_POST['email'];
 $senha = $_POST['senha'];
-echo $email, $senha;
+
+// Dados para conexão com o banco de dados
+$servidor = 'localhost';
+$userName = 'root';
+$password = 'Root';
+$dataBase = 'ingressofit';
+
+// Conectar ao banco de dados
+$connect = mysqli_connect($servidor, $userName, $password, $dataBase);
+
+// Verificar a conexão
+if (!$connect) {
+    die("Falha na conexão: " . mysqli_connect_error());
+}
+
+// Comando SQL
+$sql = "SELECT * FROM usuario WHERE email = '$email'";
+
+// Executar a consulta
+$result = mysqli_query($connect, $sql);
+
+// Verificar se a consulta retornou algum resultado
+if (mysqli_num_rows($result) > 0) {
+    // O email existe na tabela
+    while ($row = mysqli_fetch_assoc($result)) {
+        // Aqui, você pode acessar os dados do usuário
+        $nome = $row['nome'];
+        $userEmail = $row['email'];
+        $userSenha = $row['senha'];
+        if($senha == $userSenha){
+            echo $userEmail;
+        }else {
+            echo "usuário ou senha invalodo!";
+        }
+    }
+} else {
+    echo "Usuário não encontrado!";
+}
+
+// Fechar a conexão com o banco de dados
+mysqli_close($connect);
 ?>
+
