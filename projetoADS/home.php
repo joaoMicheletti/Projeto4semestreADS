13d8ea2c0d9ff46d312f6d560d07a2f2c2cdf099
Micheletti <jv604014@gmail.com>
2023-10-14 19:39:54 -0300

    adicionando ingresso ao banco de dados

--- projetoADS/home.php
+++ projetoADS/home.php
@@ -1,17 +1,29 @@
 <?php
 // variáveis  referente ao banco de dados;
-//$servidor = 'localhost';
-//$userName = 'root';
-//$password = 'Root';
-//$database = 'nome do banco';
-// conexão com o banco de dados;
-//$connect = mysqli_connect($servidor, $userName, $password, $database);
+$servidor = 'localhost';
+$userName = 'root';
+$password = 'Root';
+$database = 'ingressofit';
 
+$ususario = $_POST['sessionId'];
 $img = $_POST['img'];
-$show = $_POST['show'];
+$nome_show = $_POST['show'];
 $horaData = $_POST['horaData'];
-$local = $_POST['local'];
-$preco = $_POST['preco'];
+$local = $_POST['local']; // não sera armazenado no database;
+$valor_total = $_POST['preco'];
 
-echo $img,$show,$horaData,$local,$preco;
+// conexão com o banco de dados;
+$connect = mysqli_connect($servidor, $userName, $password, $database);
+
+// comando sql;
+$sql = "insert into ingresos(usuario, img, nome_show, valor_total) values('$usuario', '$img', '$nome_show', '$valor_total')";
+
+$msg =  'Ingresso comprado com sucesso!';
+mysqli_query($connect, $sql);
+mysqli_connect($connect, $sql);
+if(mysqli_affected_rows($connect) > 0 ){
+    echo $msg;
+} else {
+    echo "Não foi possível executar a operação!";
+};
 ?>
