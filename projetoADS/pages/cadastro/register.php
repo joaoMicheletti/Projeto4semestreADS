a52f5f30e243bb9860d0376f3bcd5fd16c4260b1
Micheletti <jv604014@gmail.com>
2023-10-27 22:38:13 -0300

    ajutes no front e logicas referentes a login e cadastro

--- projetoADS/pages/cadastro/register.php
+++ projetoADS/pages/cadastro/register.php
@@ -13,18 +13,25 @@ $password = 'Root';
 $dataBase = 'ingressofit';
 // create connection whith database;
 $connect = mysqli_connect($servido,$userName,$password,$dataBase);
+//comando de verificação;
+$verificar = "SELECT * FROM `usuario` WHERE email = '$email'";
 //commando sql;
 $sql = "insert into usuario(nome, telefone, email, senha) values('$nome','$phone', '$email', '$pass')";
 //mensagen de sucesso.
 $msg = "Dados  cadastrado com sucesso.";
 
 // iniciando a conexão.
-mysqli_query($connect, $sql);
+mysqli_query($connect, $verificar);
 
 //verificar ocorrencia;
-if(mysqli_affected_rows($connect)>0){
+if(mysqli_affected_rows($connect)==0){
+    mysqli_query($connect, $sql);
+    if(mysqli_affected_rows($connect)>0){
     echo $msg;
+    } else {
+        print "Operação não Realizada";
+    }
 } else {
-    echo "Não foi possível executar a operação!";
+    echo "operação não permitida, Usuário já cadastrado!";
 }
 ?>
--- projetoADS/pages/profile/profileIngresso.php
+++ projetoADS/pages/profile/profileIngresso.php
@@ -1,5 +1,4 @@
 <?php
-
 $servidor = "localhost";
 $userName = "root";
 $password = "Root";
@@ -7,27 +6,31 @@ $dataBase = "ingressofit";
 
 $email = $_POST['sessionId'];
 
-//conexão com o banco de dados;
+// Conexão com o banco de dados;
 $connect = mysqli_connect($servidor, $userName, $password, $dataBase);
-//comando para buscar os ingressos do usuário;
+// Comando para buscar os ingressos do usuário;
 $ingressos = "SELECT * FROM ingresos WHERE usuario = '$email'";
 
-//pegando do banco os ingressos;
+// Pegando os ingressos do banco;
 $resultIngresso = mysqli_query($connect, $ingressos);
 
+// Array para armazenar os ingressos
+$ingressosArray = array();
+
 if (mysqli_num_rows($resultIngresso) > 0) {
-    // pegando os dados de cada linha retornada da tabela ingresso;
+    // Pegando os dados de cada linha retornada da tabela ingresso;
     while ($row = mysqli_fetch_assoc($resultIngresso)) {
-        // Aqui, você pode acessar os dados do usuário
+        // Aqui, você pode acessar os dados do ingresso
         $img = $row['img'];
         $nome_show = $row['nome_show'];
-        //vou enviar ao front as variavei dentro de uma strng.
-        //com o intuito de usar o .split do js para quebralas um determinado ponto.
-        echo "{ $img'/$nome_show }";
-    };
+        
+        // Adicione os dados do ingresso ao array
+        $ingressosArray[] = array("img" => $img, "nome_show" => $nome_show);
+    }
+    
+    // Enviar a resposta JSON com os ingressos
+    echo json_encode($ingressosArray);
 } else {
-    echo "Erro ao buscar os ingressos;";
+    echo json_encode(array("error" => "Erro ao buscar os ingressos"));
 }
-
-
 ?>
